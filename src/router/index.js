b1455ebdc425ba2f8ff918630b9a915855f6d785
Benjamin Besse <benjamin.besse@aquassay.com>
2020-04-01 14:01:33 +0200

    route to stats page

--- src/router/index.js
+++ src/router/index.js
@@ -6,18 +6,16 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path        : '/',
+    name        : 'Home',
+    component   : Home
   },
-//   {
-//     path: '/about',
-//     name: 'About',
-//     // route level code-splitting
-//     // this generates a separate chunk (about.[hash].js) for this route
-//     // which is lazy-loaded when the route is visited.
-//     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-//   }
+  {
+      path      : '/stats/:country',
+      name      : 'Stats',
+      component : () => import(/* webpackChunkName: "stats" */ '../views/Stats.vue'),
+      props     : true,
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Stats.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+        Statistiques {{ country }}
+    </v-container>
+</template>
+
+<script>
+export default {
+    name : 'Stats',
+    props : {
+        country : {
+            type        : String,
+            required    : true,
+        },
+    },
+};
+</script>
